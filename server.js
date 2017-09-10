a6ec4728895de0ead7bc56f2630e95d9ae00637a
Fernando Fragoso <fernandofragoso@gmail.com>
2017-09-10 18:11:24 -0300

    Enable CORS

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ mongoose.connect("mongodb://nodetodolistapi:nodetodo123@ds131384.mlab.com:31384/
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
 
